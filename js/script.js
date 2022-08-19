e6efd7bd290ab2ef361d2b7e2fd568c80cab9496
CoderAriyan <111106728+CoderAriyan@users.noreply.github.com>
2022-08-19 17:08:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // text-animation
+    $('.hero-text-box').animatedHeadline({
+
+        animationType: 'clip'
+    })
+
+    // wow-animation
+    new WOW().init();
+})
